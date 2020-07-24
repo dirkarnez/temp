ac27fa1d57cf2429ac947355825b64724f275610
dirkarnez <smalldirkalex@gmail.com>
2020-07-24 12:53:53 +0800

    123

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+)
 
 func main() {
-    fmt.Println("hello world v8")
+	fmt.Println("hello world v8")
+	signalChan := make(chan os.Signal, 1)
+	cleanupDone := make(chan bool)
+	signal.Notify(signalChan, os.Interrupt)
+	go func() {
+		for range signalChan {
+			fmt.Printf("\nReceived an interrupt...\n\n")
+			cleanupDone <- true
+		}
+	}()
+	<-cleanupDone
 }
